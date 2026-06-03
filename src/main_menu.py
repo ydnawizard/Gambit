fa799bd3d6d0b8a9e72f5a4a860de3f486f402e8
Andy <ydnawizard@protonmail.com>
2026-06-03 09:12:46 -0500

    main menu now takes window size into account

--- src/main_menu.py
+++ src/main_menu.py
@@ -10,7 +10,8 @@ from textualeffects.widgets import EffectLabel
 gambit_header_text = ascii_art.gambit_header
 gambit_header_effect: EffectType = "Rain"
 gambit_header_config = {}
-Gambit_Header = EffectLabel(gambit_header_text, effect=gambit_header_effect)
+gambit_header = EffectLabel(gambit_header_text, effect=gambit_header_effect)
+
 
 #define play button animation
 #play_button_text = "                     PLAY\n"
@@ -19,7 +20,7 @@ play_button_effect: EffectType = "Slide"
 play_button_config = {
         "movement_speed": 0.9,
         }
-Play_Button = EffectLabel(play_button_text, effect=play_button_effect, config=play_button_config)
+play_button = EffectLabel(play_button_text, effect=play_button_effect, config=play_button_config)
 
 
 #info_button_text = "                     INFO\n"
@@ -28,7 +29,7 @@ info_button_effect: EffectType = "Slide"
 info_button_config = {
         "movement_speed": 0.6,
         }
-Info_Button = EffectLabel(info_button_text, effect=info_button_effect, config=info_button_config)
+info_button = EffectLabel(info_button_text, effect=info_button_effect, config=info_button_config)
 
 #quit_button_text = "                     QUIT\n"
 quit_button_text = ascii_art.quit_button
@@ -36,55 +37,23 @@ quit_button_effect: EffectType = "Slide"
 quit_button_config = {
         "movement_speed": 0.3,
         }
-Quit_Button = EffectLabel(quit_button_text, effect=quit_button_effect, config=quit_button_config)
+quit_button = EffectLabel(quit_button_text, effect=quit_button_effect, config=quit_button_config)
 
 class MyApp(App):
-    CSS_PATH = "grid_layout.tcss"
+    async def on_mount(self) -> None:
+        if self.screen.size.width > 150:
+            header_width = round(self.screen.size.width/3)+ self.screen.size.width//15
+            button_width = round(self.screen.size.width/3)+ self.screen.size.width//14
+        else:
+            header_width = round(self.screen.size.width/3)
+            button_width = round(self.screen.size.width/3)
+        header_height = self.screen.size.height//5
+        gambit_header.styles.margin = header_height, header_width, 0, header_width
+        play_button.styles.margin = 0, button_width, 0, button_width
+        info_button.styles.margin = 0, button_width, 0, button_width
+        quit_button.styles.margin = 0, button_width, 0, button_width
     def compose(self) -> ComposeResult:
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        #Middle
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        #New Line
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        #Middle
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        #New Line
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        #Middle
-        yield Gambit_Header
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        #New Line
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        #Middle
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        #New Line
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        #Middle LIne
-        yield Play_Button
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        #Middle Line 4
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        yield Info_Button
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        #New Line
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-        yield Quit_Button
-        yield Static("", classes="box")
-        yield Static("", classes="box")
-
+        yield gambit_header
+        yield play_button
+        yield info_button
+        yield quit_button
